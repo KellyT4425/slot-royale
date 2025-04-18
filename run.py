d0bdc4b9ff6abcb6d05e1e95b2413e78eb695461
KellyT4425 <k.thompson4421@yahoo.com>
2025-04-18 12:09:55 +0100

    feat: welcome user function was added to alow user to enter there name and recieve and welcome message, along with brief instructions

--- run.py
+++ run.py
@@ -1,3 +1,34 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+def welcome_user():
+    """
+    This function asks for an input from the user and display a welcome message, followed by
+    brief instructions on the game.
+    """
+    name = input("Please enter your name: \n")
+    print(f"Welcome {name} this is Slot Royale.\n")
+    print("The game is simple, deposit money to your account, make a bet and test your LUCK.")
+    print("You have 3 attempts to win, lets get started.")
+    return name 
+
+def exception_error():
+    """
+    This function will ensure that the users inputs are valid, for name letters/chars are accepted
+    for bets and money deposits ints are correct values in floating-point format.
+    """
+
+def user_deposit():
+    """
+    This function allows the user to input a deposit in other to place bets
+    """
+    balance = 0
+    deposit = input("Please make a deposit: £ \n")
+    print(f"Your balance is: £{deposit}")
+    return deposit
+
+def main():
+   welcome_user() 
+   user_deposit()
+
+
+main()
